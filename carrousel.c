e35deaf9292bb06028862511de796394142ed90e
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-01-21 20:03:30 +0100

    Add structure to handle carrousel items

--- carrousel.c
+++ carrousel.c
@@ -2,11 +2,22 @@
 
 #include "carrousel.h"
 
+typedef struct _Carrousel_Item Carrousel_Item;
+
+struct _Carrousel_Item
+{
+    Evas_Object *obj;
+    Evas_Object *parent;
+    Evas_Object *img;
+    double angle;
+    const char *filename;
+};
+
 Evas_Object *
 carrousel_add(Evas_Object *parent)
 {
     Evas_Object *grid;
-
+    
     grid =  elm_grid_add(parent);
     evas_object_grid_size_set(grid, 800, 600);
 
