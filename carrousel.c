02b23bc222ab657f8ce47abdd5f314323d1f7574
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-01-21 23:58:51 +0100

    Create 6 objects and Carrousel_Item related structure, and add them in the grid

--- carrousel.c
+++ carrousel.c
@@ -2,6 +2,8 @@
 
 #include "carrousel.h"
 
+#define NB_ITEMS 7
+
 typedef struct _Carrousel_Item Carrousel_Item;
 
 struct _Carrousel_Item
@@ -24,13 +26,30 @@ static const char *files[] = {
     "volaudessus.jpg"
 };
 
+
+
 Evas_Object *
 carrousel_add(Evas_Object *parent)
 {
+    int i;
     Evas_Object *grid;
-    
+    Evas_Object *ly;
+    Carrousel_Item *item;
+    char buf[PATH_MAX];
+
     grid =  elm_grid_add(parent);
     evas_object_grid_size_set(grid, 800, 600);
 
+    for (i = NB_ITEMS ; i >= 0; i--)
+    {
+        item = calloc(1, sizeof(Carrousel_Item));
+        item->parent = grid;
+        item->obj = elm_layout_add(grid);
+        elm_layout_file_set(item->obj, PACKAGE_DATA_DIR"/themes/default/default.edj",
+                            "caroussel/layout/item");
+        evas_object_show(item->obj);
+        elm_grid_pack(grid, item->obj, 0, 0, 0, 0);
+
+    }
     return grid;
 }
